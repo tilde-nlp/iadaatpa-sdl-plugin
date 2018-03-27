20c54898482c639056f742c110cd0e6736c0630e
Rihards Krišlauks <rihards.krislauks@tilde.lv>
2018-03-27 16:35:24 +0300

    Use the real iADAATPA api

--- iADAATPA.MTProvider/PluginResources.Designer.cs
+++ iADAATPA.MTProvider/PluginResources.Designer.cs
@@ -61,7 +61,7 @@ namespace iADAATPA.MTProvider {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to https://private-anon-c698772d47-iadaatpadev.apiary-mock.com/api/.
+        ///   Looks up a localized string similar to https://iadaatpa.eu/api/.
         /// </summary>
         public static string iADAATPA_API {
             get {
