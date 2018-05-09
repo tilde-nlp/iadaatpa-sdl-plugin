7f387619e19e8342a6852b91e53f85df9cde9ead
Rihards Krišlauks <rihards.krislauks@tilde.lv>
2018-05-09 17:25:46 +0300

    Test perserving non-tagged text when converting to HTML

---
+++ iADAATPA.MTProvider.Tests/SegmentExtensionTests.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+using iADAATPA.MTProvider.Extensions;
+using Sdl.LanguagePlatform.Core;
+
+namespace iADAATPA.MTProvider.Tests
+{
+    public class SegmentExtensionTests
+    {
+        [Fact]
+        public void Convert_Plaintext_to_Html()
+        {
+            string simple = "Here is some simple text without tags.";
+            Segment wrapper = new Segment();
+            wrapper.Add(simple);
+            Assert.Equal(simple, wrapper.ToHtml());
+        }
+    }
+}
